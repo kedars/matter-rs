4460fdf87ebfd14432e6344d718f75419181d041
Kedar Sovani <kedars@gmail.com>
2021-12-27 18:42:59 +0530

    Cert: Create a cert object and add get node-id/fabric-id APIs

--- matter/src/cert.rs
+++ matter/src/cert.rs
@@ -5,30 +5,52 @@ use crate::{
 };
 use num_derive::FromPrimitive;
 
+#[derive(FromPrimitive)]
+pub enum CertTags {
+    SerialNum = 1,
+    SignAlgo = 2,
+    Issuer = 3,
+    NotBefore = 4,
+    NotAfter = 5,
+    Subject = 6,
+    PubKeyAlgo = 7,
+    EcCurveId = 8,
+    EcPubKey = 9,
+    Extensions = 10,
+    Signature = 11,
+}
+
+const MC_TAG_DN_NODE_ID: u8 = 17;
+const MC_TAG_DN_FW_SIGN_ID: u8 = 18;
+const MC_TAG_DN_ICA_ID: u8 = 19;
+const MC_TAG_DN_ROOT_CA_ID: u8 = 20;
+const MC_TAG_DN_FABRIC_ID: u8 = 21;
+const MC_TAG_DN_NOC_CAT: u8 = 22;
+
 #[derive(FromPrimitive, Debug)]
-pub enum EcCurveId {
+pub enum EcCurveIdValue {
     Prime256V1 = 1,
 }
 
-pub fn get_ec_curve_id(algo: u8) -> Option<EcCurveId> {
+pub fn get_ec_curve_id(algo: u8) -> Option<EcCurveIdValue> {
     num::FromPrimitive::from_u8(algo)
 }
 
 #[derive(FromPrimitive, Debug)]
-pub enum PubKeyAlgo {
+pub enum PubKeyAlgoValue {
     EcPubKey = 1,
 }
 
-pub fn get_pubkey_algo(algo: u8) -> Option<PubKeyAlgo> {
+pub fn get_pubkey_algo(algo: u8) -> Option<PubKeyAlgoValue> {
     num::FromPrimitive::from_u8(algo)
 }
 
 #[derive(FromPrimitive, Debug)]
-pub enum SignAlgo {
+pub enum SignAlgoValue {
     ECDSAWithSHA256 = 1,
 }
 
-pub fn get_sign_algo(algo: u8) -> Option<SignAlgo> {
+pub fn get_sign_algo(algo: u8) -> Option<SignAlgoValue> {
     num::FromPrimitive::from_u8(algo)
 }
 
@@ -139,13 +161,14 @@ pub fn print_dn_list(t: TLVElement) -> Result<(), Error> {
     for t in iter {
         if let TagType::Context(tag) = t.get_tag() {
             match tag {
-                17 => println!("    Chip Node Id = {:x?}", t.get_u32()?),
-                18 => println!("    Chip Firmware Signing Id = {:?}", t.get_u8()?),
-                19 => println!("    Chip ICA Id = {:?}", t.get_u8()?),
-                20 => println!("    Chip Root CA Id = {:?}", t.get_u8()?),
-                21 => println!("    Chip Fabric Id = {:?}", t.get_u8()?),
-                22 => println!("    Chip NOC AT1 = {:?}", t.get_u8()?),
-                23 => println!("    Chip NOC AT2 = {:?}", t.get_u8()?),
+                MC_TAG_DN_NODE_ID => println!("    Chip Node Id = {:x?}", t.get_u32()?),
+                MC_TAG_DN_FW_SIGN_ID => {
+                    println!("    Chip Firmware Signing Id = {:?}", t.get_u8()?)
+                }
+                MC_TAG_DN_ICA_ID => println!("    Chip ICA Id = {:?}", t.get_u8()?),
+                MC_TAG_DN_ROOT_CA_ID => println!("    Chip Root CA Id = {:?}", t.get_u8()?),
+                MC_TAG_DN_FABRIC_ID => println!("    Chip Fabric Id = {:?}", t.get_u8()?),
+                MC_TAG_DN_NOC_CAT => println!("    Chip NOC CAT = {:?}", t.get_u8()?),
                 _ => println!("Unsupported tag"),
             }
         }
@@ -153,39 +176,73 @@ pub fn print_dn_list(t: TLVElement) -> Result<(), Error> {
     Ok(())
 }
 
+pub struct Cert(Vec<u8>);
+
+impl Cert {
+    pub fn new(cert_bin: &[u8]) -> Self {
+        Self(cert_bin.to_vec())
+    }
+
+    pub fn get_node_id(&self) -> Result<u64, Error> {
+        tlv::get_root_node_struct(self.0.as_slice())?
+            .find_tag(CertTags::Subject as u32)?
+            .confirm_list()?
+            .find_tag(MC_TAG_DN_NODE_ID as u32)
+            .map_err(|_e| Error::NoNodeId)?
+            .get_u32()
+            .map(|e| e as u64)
+    }
+
+    pub fn get_fabric_id(&self) -> Result<u64, Error> {
+        tlv::get_root_node_struct(self.0.as_slice())?
+            .find_tag(CertTags::Subject as u32)?
+            .confirm_list()?
+            .find_tag(MC_TAG_DN_FABRIC_ID as u32)
+            .map_err(|_e| Error::NoFabricId)?
+            .get_u8()
+            .map(|e| e as u64)
+    }
+}
+
+impl Default for Cert {
+    fn default() -> Self {
+        Self(Vec::with_capacity(0))
+    }
+}
+
 pub fn print_cert(buf: &[u8]) -> Result<(), Error> {
     let iter = tlv::get_root_node_struct(buf)?.iter().unwrap();
 
     for t in iter {
         if let TagType::Context(tag) = t.get_tag() {
+            let tag = num::FromPrimitive::from_u8(tag).ok_or(Error::InvalidData)?;
             match tag {
-                1 => println!("Serial Number: {:x?}", t.get_slice()?),
-                2 => println!(
+                CertTags::SerialNum => println!("Serial Number: {:x?}", t.get_slice()?),
+                CertTags::SignAlgo => println!(
                     "Signature Algorithm: {:?}",
                     get_sign_algo(t.get_u8()?).ok_or(Error::Invalid)?
                 ),
-                3 => {
+                CertTags::Issuer => {
                     println!("Issuer:");
                     print_dn_list(t)?;
                 }
-                4 => println!("Not Before: {:?}", t.get_u32()?),
-                5 => println!("Not After: {:?}", t.get_u32()?),
-                6 => {
+                CertTags::NotBefore => println!("Not Before: {:?}", t.get_u32()?),
+                CertTags::NotAfter => println!("Not After: {:?}", t.get_u32()?),
+                CertTags::Subject => {
                     println!("Subject:");
                     print_dn_list(t)?;
                 }
-                7 => println!(
+                CertTags::PubKeyAlgo => println!(
                     "Public Key Algorithm: {:?}",
                     get_pubkey_algo(t.get_u8()?).ok_or(Error::Invalid)?,
                 ),
-                8 => println!(
+                CertTags::EcCurveId => println!(
                     "Elliptic Curve: {:?}",
                     get_ec_curve_id(t.get_u8()?).ok_or(Error::Invalid)?
                 ),
-                9 => println!("Public-Key: {:?}", t.get_slice()?),
-                10 => print_extensions(t)?,
-                11 => println!("Signature: {:x?}", t.get_slice()?),
-                _ => println!("Unsupported tag\n"),
+                CertTags::EcPubKey => println!("Public-Key: {:?}", t.get_slice()?),
+                CertTags::Extensions => print_extensions(t)?,
+                CertTags::Signature => println!("Signature: {:x?}", t.get_slice()?),
             }
         }
     }
