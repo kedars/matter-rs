4747b65a850871507c39253297f78d956892d1d0
Kedar Sovani <kedars@gmail.com>
2022-01-24 19:00:41 +0530

    Cert: Restructuring for easier usage

--- matter/src/cert/mod.rs
+++ matter/src/cert/mod.rs
@@ -5,9 +5,10 @@ use crate::{
     tlv::{self, TLVContainerIterator, TLVElement},
     tlv_common::TagType,
 };
-use chrono::{TimeZone, Utc};
 use log::error;
 use num_derive::FromPrimitive;
+
+use self::{asn1_writer::ASN1Writer, printer::CertPrinter};
 
 // As per https://datatracker.ietf.org/doc/html/rfc5280
 
@@ -109,7 +110,7 @@ fn get_print_str(key_usage: u16) -> String {
 }
 
 #[allow(unused_assignments)]
-pub fn decode_key_usage(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_key_usage(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
     // TODO This should be u16, but we get u8 for now
     let key_usage = t.get_u8()? as u16;
     let mut key_usage_str = [0u8; 2];
@@ -118,7 +119,7 @@ pub fn decode_key_usage(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), E
     Ok(())
 }
 
-pub fn decode_extended_key_usage(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_extended_key_usage(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
     const OID_SERVER_AUTH: [u8; 8] = [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x03, 0x01];
     const OID_CLIENT_AUTH: [u8; 8] = [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x03, 0x02];
     const OID_CODE_SIGN: [u8; 8] = [0x2B, 0x06, 0x01, 0x05, 0x05, 0x07, 0x03, 0x03];
@@ -168,7 +169,7 @@ pub fn decode_basic_constraints(t: TLVElement, w: &mut dyn CertConsumer) -> Resu
     w.end_seq()
 }
 
-pub fn decode_extension_start(
+fn decode_extension_start(
     tag: &str,
     critical: bool,
     oid: &[u8],
@@ -182,13 +183,13 @@ pub fn decode_extension_start(
     w.start_compound_ostr("value:")
 }
 
-pub fn decode_extension_end(w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_extension_end(w: &mut dyn CertConsumer) -> Result<(), Error> {
     w.end_compound_ostr()?;
     w.end_seq()
 }
 
 #[derive(FromPrimitive)]
-pub enum ExtTags {
+enum ExtTags {
     BasicConstraints = 1,
     KeyUsage = 2,
     ExtKeyUsage = 3,
@@ -196,7 +197,7 @@ pub enum ExtTags {
     AuthKeyId = 5,
     FutureExt = 6,
 }
-pub fn decode_extensions(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_extensions(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
     const OID_BASIC_CONSTRAINTS: [u8; 3] = [0x55, 0x1D, 0x13];
     const OID_KEY_USAGE: [u8; 3] = [0x55, 0x1D, 0x0F];
     const OID_EXT_KEY_USAGE: [u8; 3] = [0x55, 0x1D, 0x25];
@@ -259,7 +260,7 @@ pub fn decode_extensions(t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), 
 }
 
 #[derive(FromPrimitive)]
-pub enum DnTags {
+enum DnTags {
     NodeId = 17,
     FirmwareSignId = 18,
     IcaId = 19,
@@ -267,7 +268,7 @@ pub enum DnTags {
     FabricId = 21,
     NocCat = 22,
 }
-pub fn decode_dn_list(tag: &str, t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_dn_list(tag: &str, t: TLVElement, w: &mut dyn CertConsumer) -> Result<(), Error> {
     const OID_MATTER_NODE_ID: [u8; 10] =
         [0x2B, 0x06, 0x01, 0x04, 0x01, 0x82, 0xA2, 0x7C, 0x01, 0x01];
     const OID_MATTER_FW_SIGN_ID: [u8; 10] =
@@ -344,7 +345,7 @@ fn get_next_tag<'a>(
     }
 }
 
-pub fn decode_cert(buf: &[u8], w: &mut dyn CertConsumer) -> Result<(), Error> {
+fn decode_cert(buf: &[u8], w: &mut dyn CertConsumer) -> Result<(), Error> {
     let mut iter = tlv::get_root_node_struct(buf)?.iter().unwrap();
 
     w.start_seq("")?;
@@ -474,6 +475,12 @@ impl Cert {
     pub fn as_slice(&self) -> Result<&[u8], Error> {
         Ok(self.0.as_slice())
     }
+
+    pub fn as_asn1(&self, buf: &mut [u8]) -> Result<usize, Error> {
+        let mut w = ASN1Writer::new(buf);
+        let _ = decode_cert(self.0.as_slice(), &mut w)?;
+        Ok(w.as_slice().len())
+    }
 }
 
 impl Default for Cert {
@@ -516,366 +523,14 @@ pub trait CertConsumer {
     fn utctime(&mut self, tag: &str, epoch: u32) -> Result<(), Error>;
 }
 
-pub struct CertPrinter<'a, 'b> {
-    level: usize,
-    f: &'b mut fmt::Formatter<'a>,
-}
-
-impl<'a, 'b> CertPrinter<'a, 'b> {
-    pub fn new(f: &'b mut fmt::Formatter<'a>) -> Self {
-        Self { level: 0, f }
-    }
-}
-
 const MAX_DEPTH: usize = 10;
-const SPACE: [&str; MAX_DEPTH] = [
-    "",
-    "",
-    "    ",
-    "        ",
-    "            ",
-    "                ",
-    "                    ",
-    "                        ",
-    "                            ",
-    "                                ",
-];
 
-impl<'a, 'b> CertConsumer for CertPrinter<'a, 'b> {
-    fn start_seq(&mut self, tag: &str) -> Result<(), Error> {
-        if tag.len() != 0 {
-            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
-        }
-        self.level += 1;
-        Ok(())
-    }
-    fn end_seq(&mut self) -> Result<(), Error> {
-        self.level -= 1;
-        Ok(())
-    }
-    fn integer(&mut self, tag: &str, i: &[u8]) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, i);
-        Ok(())
-    }
-    fn utf8str(&mut self, tag: &str, s: &str) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
-        Ok(())
-    }
-    fn bitstr(&mut self, tag: &str, _truncate: bool, s: &[u8]) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
-        Ok(())
-    }
-    fn ostr(&mut self, tag: &str, s: &[u8]) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
-        Ok(())
-    }
-    fn start_compound_ostr(&mut self, tag: &str) -> Result<(), Error> {
-        if tag.len() != 0 {
-            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
-        }
-        self.level += 1;
-        Ok(())
-    }
-    fn end_compound_ostr(&mut self) -> Result<(), Error> {
-        self.level -= 1;
-        Ok(())
-    }
-    fn bool(&mut self, tag: &str, b: bool) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} {}", SPACE[self.level], tag, b);
-        Ok(())
-    }
-    fn start_set(&mut self, tag: &str) -> Result<(), Error> {
-        if tag.len() != 0 {
-            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
-        }
-        self.level += 1;
-        Ok(())
-    }
-    fn end_set(&mut self) -> Result<(), Error> {
-        self.level -= 1;
-        Ok(())
-    }
-    fn ctx(&mut self, tag: &str, id: u8, val: &[u8]) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {}[{}]{:x?}", SPACE[self.level], tag, id, val);
-        Ok(())
-    }
-    fn start_ctx(&mut self, tag: &str, val: u8) -> Result<(), Error> {
-        let _ = writeln!(self.f, "{} {} [{}]", SPACE[self.level], tag, val);
-        self.level += 1;
-        Ok(())
-    }
-    fn end_ctx(&mut self) -> Result<(), Error> {
-        self.level -= 1;
-        Ok(())
-    }
-    fn oid(&mut self, tag: &str, _oid: &[u8]) -> Result<(), Error> {
-        if tag.len() != 0 {
-            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
-        }
-        Ok(())
-    }
-    fn utctime(&mut self, tag: &str, epoch: u32) -> Result<(), Error> {
-        let mut matter_epoch = Utc.ymd(2000, 1, 1).and_hms(0, 0, 0).timestamp();
-        matter_epoch += epoch as i64;
-        let _ = writeln!(
-            self.f,
-            "{} {} {}",
-            SPACE[self.level],
-            tag,
-            Utc.timestamp(matter_epoch, 0)
-        );
-        Ok(())
-    }
-}
-
-#[derive(Debug)]
-pub struct ASN1Writer<'a> {
-    buf: &'a mut [u8],
-    // The current write offset in the buffer
-    offset: usize,
-    // If multiple 'composite' structures are being written, their starts are
-    // captured in this
-    depth: [usize; MAX_DEPTH],
-    // The current depth of operation within the depth stack
-    current_depth: usize,
-}
-
-const RESERVE_LEN_BYTES: usize = 3;
-impl<'a> ASN1Writer<'a> {
-    pub fn new(buf: &'a mut [u8]) -> Self {
-        Self {
-            buf,
-            offset: 0,
-            depth: [0; MAX_DEPTH],
-            current_depth: 0,
-        }
-    }
-
-    pub fn append_with<F>(&mut self, size: usize, f: F) -> Result<(), Error>
-    where
-        F: FnOnce(&mut Self),
-    {
-        if self.offset + size <= self.buf.len() {
-            f(self);
-            self.offset += size;
-            return Ok(());
-        }
-        Err(Error::NoSpace)
-    }
-
-    pub fn append_tlv<F>(&mut self, tag: u8, len: usize, f: F) -> Result<(), Error>
-    where
-        F: FnOnce(&mut Self),
-    {
-        let total_len = 1 + ASN1Writer::bytes_to_encode_len(len)? + len;
-        if self.offset + total_len <= self.buf.len() {
-            self.buf[self.offset] = tag;
-            self.offset += 1;
-            self.offset = self.encode_len(self.offset, len)?;
-            f(self);
-            self.offset += len;
-            return Ok(());
-        }
-        Err(Error::NoSpace)
-    }
-
-    fn add_compound(&mut self, val: u8) -> Result<(), Error> {
-        // We reserve 3 bytes for encoding the length
-        // If a shorter length is actually required, we will move everything back
-        self.append_with(1 + RESERVE_LEN_BYTES, |t| t.buf[t.offset] = val)?;
-        self.depth[self.current_depth] = self.offset;
-        self.current_depth += 1;
-        if self.current_depth >= MAX_DEPTH {
-            Err(Error::NoSpace)
-        } else {
-            Ok(())
-        }
-    }
-
-    fn encode_len(&mut self, mut at_offset: usize, len: usize) -> Result<usize, Error> {
-        let mut bytes_of_len = ASN1Writer::bytes_to_encode_len(len)?;
-        if bytes_of_len > 1 {
-            self.buf[at_offset] = (0x80 | bytes_of_len - 1) as u8;
-            at_offset += 1;
-            bytes_of_len -= 1;
-        }
-
-        // At this point bytes_of_len is the actual number of bytes for the length encoding
-        // after the 0x80 (if it was present)
-        let mut octet_number = bytes_of_len - 1;
-        // We start encoding the highest octest first
-        loop {
-            self.buf[at_offset] = ((len >> (octet_number * 8)) & 0xff) as u8;
-
-            at_offset += 1;
-            if octet_number == 0 {
-                break;
-            }
-            octet_number -= 1;
-        }
-
-        Ok(at_offset)
-    }
-
-    fn end_compound(&mut self) -> Result<(), Error> {
-        if self.current_depth == 0 {
-            return Err(Error::Invalid);
-        }
-        let seq_len = self.get_compound_len();
-        let write_offset = self.get_length_encoding_offset();
-
-        let mut write_offset = self.encode_len(write_offset, seq_len)?;
-
-        // Shift everything by as much
-        let shift_len = self.depth[self.current_depth - 1] - write_offset;
-        if shift_len > 0 {
-            for _i in 0..seq_len {
-                self.buf[write_offset] = self.buf[write_offset + shift_len];
-                write_offset += 1;
-            }
-        }
-        self.current_depth -= 1;
-        self.offset -= shift_len;
-        Ok(())
-    }
-
-    fn get_compound_len(&self) -> usize {
-        self.offset - self.depth[self.current_depth - 1]
-    }
-
-    fn bytes_to_encode_len(len: usize) -> Result<usize, Error> {
-        let len = if len < 128 {
-            // This is directly encoded
-            1
-        } else if len < 256 {
-            // This is done with an 0xA1 followed by actual len
-            2
-        } else if len < 65536 {
-            // This is done with an 0xA2 followed by 2 bytes of actual len
-            3
-        } else {
-            return Err(Error::NoSpace);
-        };
-        Ok(len)
-    }
-
-    fn get_length_encoding_offset(&self) -> usize {
-        self.depth[self.current_depth - 1] - RESERVE_LEN_BYTES
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.buf[..self.offset]
-    }
-
-    fn write_str(&mut self, vtype: u8, s: &[u8]) -> Result<(), Error> {
-        self.append_tlv(vtype, s.len(), |t| {
-            let end_offset = t.offset + s.len();
-            t.buf[t.offset..end_offset].copy_from_slice(s);
-        })
-    }
-}
-
-impl<'a> CertConsumer for ASN1Writer<'a> {
-    fn start_seq(&mut self, _tag: &str) -> Result<(), Error> {
-        self.add_compound(0x30)
-    }
-
-    fn end_seq(&mut self) -> Result<(), Error> {
-        self.end_compound()
-    }
-
-    fn integer(&mut self, _tag: &str, i: &[u8]) -> Result<(), Error> {
-        self.write_str(0x02, i)
-    }
-
-    fn utf8str(&mut self, _tag: &str, s: &str) -> Result<(), Error> {
-        // Note: ASN1 has 3 string, this is UTF8String
-        self.write_str(0x0c, s.as_bytes())
-    }
-
-    fn bitstr(&mut self, _tag: &str, truncate: bool, s: &[u8]) -> Result<(), Error> {
-        // Note: ASN1 has 3 string, this is BIT String
-
-        // Strip off the end zeroes
-        let mut last_byte = s.len() - 1;
-        let mut num_of_zero = 0;
-        if truncate {
-            while s[last_byte] == 0 {
-                last_byte -= 1;
-            }
-            // For the last valid byte, identifying the number of last bits
-            // that are 0s
-            num_of_zero = s[last_byte].trailing_zeros() as u8;
-        }
-        let s = &s[..(last_byte + 1)];
-        self.append_tlv(0x03, s.len() + 1, |t| {
-            t.buf[t.offset] = num_of_zero;
-            let end_offset = t.offset + 1 + s.len();
-            t.buf[(t.offset + 1)..end_offset].copy_from_slice(s);
-        })
-    }
-
-    fn ostr(&mut self, _tag: &str, s: &[u8]) -> Result<(), Error> {
-        // Note: ASN1 has 3 string, this is Octet String
-        self.write_str(0x04, s)
-    }
-
-    fn start_compound_ostr(&mut self, _tag: &str) -> Result<(), Error> {
-        // Note: ASN1 has 3 string, this is compound Octet String
-        self.add_compound(0x04)
-    }
-
-    fn end_compound_ostr(&mut self) -> Result<(), Error> {
-        self.end_compound()
-    }
-
-    fn bool(&mut self, _tag: &str, b: bool) -> Result<(), Error> {
-        self.append_tlv(0x01, 1, |t| {
-            if b {
-                t.buf[t.offset] = 0xFF;
-            } else {
-                t.buf[t.offset] = 0x00;
-            }
-        })
-    }
-
-    fn start_set(&mut self, _tag: &str) -> Result<(), Error> {
-        self.add_compound(0x31)
-    }
-
-    fn end_set(&mut self) -> Result<(), Error> {
-        self.end_compound()
-    }
-
-    fn ctx(&mut self, _tag: &str, id: u8, val: &[u8]) -> Result<(), Error> {
-        self.write_str(0x80 | id, val)
-    }
-
-    fn start_ctx(&mut self, _tag: &str, val: u8) -> Result<(), Error> {
-        self.add_compound(0xA0 | val)
-    }
-
-    fn end_ctx(&mut self) -> Result<(), Error> {
-        self.end_compound()
-    }
-
-    fn oid(&mut self, _tag: &str, oid: &[u8]) -> Result<(), Error> {
-        self.write_str(0x06, oid)
-    }
-
-    fn utctime(&mut self, _tag: &str, epoch: u32) -> Result<(), Error> {
-        let mut matter_epoch = Utc.ymd(2000, 1, 1).and_hms(0, 0, 0).timestamp();
-        matter_epoch += epoch as i64;
-
-        let dt = Utc.timestamp(matter_epoch, 0);
-        let time_str = format!("{}Z", dt.format("%y%m%d%H%M%S"));
-        self.write_str(0x17, time_str.as_bytes())
-    }
-}
+mod asn1_writer;
+mod printer;
 
 #[cfg(test)]
 mod tests {
-    use crate::cert::{decode_cert, ASN1Writer};
+    use crate::cert::Cert;
 
     #[test]
     fn test_success_decode() {
@@ -985,16 +640,16 @@ mod tests {
 
         {
             let mut asn1_buf = [0u8; 1000];
-            let mut w = ASN1Writer::new(&mut asn1_buf);
-            decode_cert(&test_input1, &mut w).unwrap();
-            assert_eq!(&test_output1, w.as_slice());
+            let c = Cert::new(&test_input1);
+            let len = c.as_asn1(&mut asn1_buf).unwrap();
+            assert_eq!(&test_output1, &asn1_buf[..len]);
         }
 
         {
             let mut asn1_buf = [0u8; 1000];
-            let mut w = ASN1Writer::new(&mut asn1_buf);
-            decode_cert(&test_input2, &mut w).unwrap();
-            assert_eq!(&test_output2, w.as_slice());
+            let c = Cert::new(&test_input2);
+            let len = c.as_asn1(&mut asn1_buf).unwrap();
+            assert_eq!(&test_output2, &asn1_buf[..len]);
         }
     }
 }
---
+++ matter/src/cert/printer.rs
@@ -0,0 +1,115 @@
+use super::{CertConsumer, MAX_DEPTH};
+use crate::error::Error;
+use chrono::{TimeZone, Utc};
+use std::fmt;
+
+pub struct CertPrinter<'a, 'b> {
+    level: usize,
+    f: &'b mut fmt::Formatter<'a>,
+}
+
+impl<'a, 'b> CertPrinter<'a, 'b> {
+    pub fn new(f: &'b mut fmt::Formatter<'a>) -> Self {
+        Self { level: 0, f }
+    }
+}
+
+const SPACE: [&str; MAX_DEPTH] = [
+    "",
+    "",
+    "    ",
+    "        ",
+    "            ",
+    "                ",
+    "                    ",
+    "                        ",
+    "                            ",
+    "                                ",
+];
+
+impl<'a, 'b> CertConsumer for CertPrinter<'a, 'b> {
+    fn start_seq(&mut self, tag: &str) -> Result<(), Error> {
+        if tag.len() != 0 {
+            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
+        }
+        self.level += 1;
+        Ok(())
+    }
+    fn end_seq(&mut self) -> Result<(), Error> {
+        self.level -= 1;
+        Ok(())
+    }
+    fn integer(&mut self, tag: &str, i: &[u8]) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, i);
+        Ok(())
+    }
+    fn utf8str(&mut self, tag: &str, s: &str) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
+        Ok(())
+    }
+    fn bitstr(&mut self, tag: &str, _truncate: bool, s: &[u8]) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
+        Ok(())
+    }
+    fn ostr(&mut self, tag: &str, s: &[u8]) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} {:x?}", SPACE[self.level], tag, s);
+        Ok(())
+    }
+    fn start_compound_ostr(&mut self, tag: &str) -> Result<(), Error> {
+        if tag.len() != 0 {
+            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
+        }
+        self.level += 1;
+        Ok(())
+    }
+    fn end_compound_ostr(&mut self) -> Result<(), Error> {
+        self.level -= 1;
+        Ok(())
+    }
+    fn bool(&mut self, tag: &str, b: bool) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} {}", SPACE[self.level], tag, b);
+        Ok(())
+    }
+    fn start_set(&mut self, tag: &str) -> Result<(), Error> {
+        if tag.len() != 0 {
+            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
+        }
+        self.level += 1;
+        Ok(())
+    }
+    fn end_set(&mut self) -> Result<(), Error> {
+        self.level -= 1;
+        Ok(())
+    }
+    fn ctx(&mut self, tag: &str, id: u8, val: &[u8]) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {}[{}]{:x?}", SPACE[self.level], tag, id, val);
+        Ok(())
+    }
+    fn start_ctx(&mut self, tag: &str, val: u8) -> Result<(), Error> {
+        let _ = writeln!(self.f, "{} {} [{}]", SPACE[self.level], tag, val);
+        self.level += 1;
+        Ok(())
+    }
+    fn end_ctx(&mut self) -> Result<(), Error> {
+        self.level -= 1;
+        Ok(())
+    }
+    fn oid(&mut self, tag: &str, _oid: &[u8]) -> Result<(), Error> {
+        if tag.len() != 0 {
+            let _ = writeln!(self.f, "{} {}", SPACE[self.level], tag);
+        }
+        Ok(())
+    }
+    fn utctime(&mut self, tag: &str, epoch: u32) -> Result<(), Error> {
+        let mut matter_epoch = Utc.ymd(2000, 1, 1).and_hms(0, 0, 0).timestamp();
+        matter_epoch += epoch as i64;
+        let _ = writeln!(
+            self.f,
+            "{} {} {}",
+            SPACE[self.level],
+            tag,
+            Utc.timestamp(matter_epoch, 0)
+        );
+        Ok(())
+    }
+}
