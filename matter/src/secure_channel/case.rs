44ff1db3edd0c21d98902f80c24c979fc2dfcde7
Kedar Sovani <kedars@gmail.com>
2022-01-28 08:25:12 +0530

    CASE: Validate Sigma3 TBS

--- matter/src/secure_channel/case.rs
+++ matter/src/secure_channel/case.rs
@@ -41,6 +41,8 @@ pub struct CaseSession {
     local_sessid: u16,
     tt_hash: Sha256,
     shared_secret: [u8; crypto::ECDH_SHARED_SECRET_LEN_BYTES],
+    our_pub_key: [u8; crypto::EC_POINT_LEN_BYTES],
+    peer_pub_key: [u8; crypto::EC_POINT_LEN_BYTES],
     local_fabric_idx: usize,
 }
 impl CaseSession {
@@ -50,7 +52,9 @@ impl CaseSession {
             peer_sessid,
             local_sessid,
             tt_hash: Sha256::new(),
-            shared_secret: [0; 32],
+            shared_secret: [0; crypto::ECDH_SHARED_SECRET_LEN_BYTES],
+            our_pub_key: [0; crypto::EC_POINT_LEN_BYTES],
+            peer_pub_key: [0; crypto::EC_POINT_LEN_BYTES],
             local_fabric_idx: 0,
         })
     }
@@ -97,8 +101,8 @@ impl Case {
         trace!("Decrypted: {:x?}", decrypted);
 
         let root = get_root_node_struct(decrypted)?;
-        let initiator_noc = Cert::new(root.find_tag(1)?.get_slice()?);
-        let initiator_icac = Cert::new(root.find_tag(2)?.get_slice()?);
+        let initiator_noc_b = root.find_tag(1)?.get_slice()?;
+        let initiator_icac_b = root.find_tag(2)?.get_slice()?;
         let signature = root.find_tag(3)?.get_slice()?;
         // TODO: Signature Validation
 
@@ -111,8 +115,25 @@ impl Case {
         // Safe to unwrap here
         let fabric = fabric.as_ref().as_ref().unwrap();
 
+        let initiator_noc = Cert::new(initiator_noc_b);
+        let initiator_icac = Cert::new(initiator_icac_b);
         if Case::validate_certs(fabric, &initiator_noc, &initiator_icac).is_err() {
             error!("Certificate Chain doesn't match");
+            common::create_sc_status_report(proto_tx, common::SCStatusCodes::InvalidParameter)?;
+            proto_rx.exchange.close();
+            return Ok(());
+        }
+
+        if Case::validate_sigma3_sign(
+            initiator_noc_b,
+            initiator_icac_b,
+            &initiator_noc,
+            signature,
+            &case_session,
+        )
+        .is_err()
+        {
+            error!("Sigma3 Signature doesn't match");
             common::create_sc_status_report(proto_tx, common::SCStatusCodes::InvalidParameter)?;
             proto_rx.exchange.close();
             return Ok(());
@@ -152,6 +173,11 @@ impl Case {
         let mut case_session = Box::new(CaseSession::new(initiator_sessid as u16, local_sessid)?);
         case_session.tt_hash.update(proto_rx.buf);
         case_session.local_fabric_idx = local_fabric_idx?;
+        if peer_pub_key.len() != crypto::EC_POINT_LEN_BYTES {
+            error!("Invalid public key length");
+            return Err(Error::Invalid);
+        }
+        case_session.peer_pub_key.copy_from_slice(peer_pub_key);
         trace!(
             "Destination ID matched to fabric index {}",
             case_session.local_fabric_idx
@@ -159,9 +185,7 @@ impl Case {
 
         // Create an ephemeral Key Pair
         let key_pair = KeyPair::new()?;
-        let mut our_pub_key = [0_u8; crypto::EC_POINT_LEN_BYTES];
-        let len = key_pair.get_public_key(&mut our_pub_key)?;
-        let our_pub_key = &our_pub_key[..len];
+        let _ = key_pair.get_public_key(&mut case_session.our_pub_key)?;
 
         // Derive the Shared Secret
         let len = key_pair.derive_secret(peer_pub_key, &mut case_session.shared_secret)?;
@@ -190,21 +214,18 @@ impl Case {
                 return Ok(());
             }
 
-            let sign_len =
-                Case::get_sigma2_signature(&fabric, our_pub_key, peer_pub_key, &mut signature)?;
+            let sign_len = Case::get_sigma2_sign(
+                &fabric,
+                &case_session.our_pub_key,
+                &case_session.peer_pub_key,
+                &mut signature,
+            )?;
             let signature = &signature[..sign_len];
 
             // TODO: Fix IPK
             let dummy_ipk = [0_u8; crypto::SYMM_KEY_LEN_BYTES];
             let mut sigma2_key = [0_u8; crypto::SYMM_KEY_LEN_BYTES];
-            Case::get_sigma2_key(
-                &dummy_ipk,
-                &our_random,
-                our_pub_key,
-                &case_session.tt_hash,
-                &case_session.shared_secret,
-                &mut sigma2_key,
-            )?;
+            Case::get_sigma2_key(&dummy_ipk, &our_random, &mut case_session, &mut sigma2_key)?;
 
             Case::get_sigma2_encryption(&fabric, &sigma2_key, signature, &mut encrypted)?
         };
@@ -215,7 +236,7 @@ impl Case {
         tw.put_start_struct(TagType::Anonymous)?;
         tw.put_str8(TagType::Context(1), &our_random)?;
         tw.put_u16(TagType::Context(2), local_sessid)?;
-        tw.put_str8(TagType::Context(3), our_pub_key)?;
+        tw.put_str8(TagType::Context(3), &case_session.our_pub_key)?;
         tw.put_str16(TagType::Context(4), encrypted)?;
         tw.put_end_container()?;
         case_session.tt_hash.update(proto_tx.write_buf.as_slice());
@@ -223,10 +244,7 @@ impl Case {
         Ok(())
     }
 
-    fn get_session_clone_data(
-        ipk: &[u8],
-        case_session: &Box<CaseSession>,
-    ) -> Result<CloneData, Error> {
+    fn get_session_clone_data(ipk: &[u8], case_session: &CaseSession) -> Result<CloneData, Error> {
         let mut session_keys = [0_u8; 3 * crypto::SYMM_KEY_LEN_BYTES];
         Case::get_session_keys(
             ipk,
@@ -246,6 +264,29 @@ impl Case {
             .att_challenge
             .copy_from_slice(&session_keys[32..48]);
         Ok(clone_data)
+    }
+
+    fn validate_sigma3_sign(
+        initiator_noc: &[u8],
+        initiator_icac: &[u8],
+        initiator_noc_cert: &Cert,
+        sign: &[u8],
+        case_session: &CaseSession,
+    ) -> Result<(), Error> {
+        const MAX_TBS_SIZE: usize = 800;
+        let mut buf: [u8; MAX_TBS_SIZE] = [0; MAX_TBS_SIZE];
+        let mut write_buf = WriteBuf::new(&mut buf, MAX_TBS_SIZE);
+        let mut tw = TLVWriter::new(&mut write_buf);
+        tw.put_start_struct(TagType::Anonymous)?;
+        tw.put_str8(TagType::Context(1), initiator_noc)?;
+        tw.put_str8(TagType::Context(2), initiator_icac)?;
+        tw.put_str8(TagType::Context(3), &case_session.peer_pub_key)?;
+        tw.put_str8(TagType::Context(4), &case_session.our_pub_key)?;
+        tw.put_end_container()?;
+
+        let key = KeyPair::new_from_public(initiator_noc_cert.get_pubkey()?)?;
+        key.verify_msg(write_buf.as_slice(), sign)?;
+        Ok(())
     }
 
     fn validate_certs(fabric: &Fabric, noc: &Cert, icac: &Cert) -> Result<(), Error> {
@@ -292,7 +333,7 @@ impl Case {
 
     fn get_sigma3_decryption(
         ipk: &[u8],
-        case_session: &Box<CaseSession>,
+        case_session: &CaseSession,
         encrypted: &mut [u8],
     ) -> Result<usize, Error> {
         let mut sigma3_key = [0_u8; crypto::SYMM_KEY_LEN_BYTES];
@@ -351,9 +392,7 @@ impl Case {
     fn get_sigma2_key(
         ipk: &[u8],
         our_random: &[u8],
-        our_pub_key: &[u8],
-        tt_hash: &Sha256,
-        shared_secret: &[u8],
+        case_session: &mut CaseSession,
         key: &mut [u8],
     ) -> Result<(), Error> {
         const S2K_INFO: [u8; 6] = [0x53, 0x69, 0x67, 0x6d, 0x61, 0x32];
@@ -363,15 +402,15 @@ impl Case {
         let mut salt = Vec::<u8>::with_capacity(256);
         salt.extend_from_slice(ipk);
         salt.extend_from_slice(our_random);
-        salt.extend_from_slice(our_pub_key);
+        salt.extend_from_slice(&case_session.our_pub_key);
 
-        let tt_hash = tt_hash.clone();
+        let tt_hash = case_session.tt_hash.clone();
         let tt_hash = tt_hash.finalize();
         let tt_hash = tt_hash.as_slice();
         salt.extend_from_slice(tt_hash);
         //        println!("Sigma2Key: salt: {:x?}, len: {}", salt, salt.len());
 
-        let h = Hkdf::<Sha256>::new(Some(salt.as_slice()), shared_secret);
+        let h = Hkdf::<Sha256>::new(Some(salt.as_slice()), &mut case_session.shared_secret);
         h.expand(&S2K_INFO, key).map_err(|_x| Error::NoSpace)?;
         //        println!("Sigma2Key: key: {:x?}", key);
 
@@ -412,7 +451,7 @@ impl Case {
         Ok(write_buf.as_slice().len())
     }
 
-    fn get_sigma2_signature(
+    fn get_sigma2_sign(
         fabric: &RwLockReadGuardRef<FabricMgrInner, Option<Fabric>>,
         our_pub_key: &[u8],
         peer_pub_key: &[u8],
