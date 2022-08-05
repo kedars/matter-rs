e2c78b3390358169565a8f185d15e8018d61b625
Kedar Sovani <kedars@gmail.com>
2022-08-05 22:34:25 +0530

    mdns: Fix Linux Build

--- matter/src/sys/sys_linux.rs
+++ matter/src/sys/sys_linux.rs
@@ -1,33 +1,14 @@
-use std::sync::{Arc, Mutex, Once};
-
 use crate::error::Error;
-
 use log::error;
 
-pub struct Mdns {}
-
-static mut G_MDNS: Option<Arc<Mutex<Mdns>>> = None;
-static INIT: Once = Once::new();
-
 #[allow(dead_code)]
-pub struct MdnsService {}
+pub struct SysMdnsService {}
 
-impl Mdns {
-    fn new() -> Self {
-        Self {}
-    }
-
-    pub fn get() -> Result<Arc<Mutex<Self>>, Error> {
-        unsafe {
-            INIT.call_once(|| {
-                G_MDNS = Some(Arc::new(Mutex::new(Mdns::new())));
-            });
-            Ok(G_MDNS.as_ref().ok_or(Error::Invalid)?.clone())
-        }
-    }
-
-    pub fn publish_service(name: &str) -> Result<MdnsService, Error> {
-        error!("Linux is not yet supported for MDNS Service");
-        Ok(MdnsService {})
-    }
+pub fn sys_publish_service(
+    _name: &str,
+    _regtype: &str,
+    _port: u16,
+) -> Result<SysMdnsService, Error> {
+    error!("Linux is not yet supported for MDNS Service");
+    Ok(SysMdnsService {})
 }
